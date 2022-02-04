a94701e112117862d83360519716c591af52881b
Cody S <cody.scoles@gmail.com>
2022-02-04 17:52:01 -0500

    Script fetches correct API information.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,84 @@
-
-
-
-
-
-
 //Reference HTML 
 var citySearch = $("#city-search");
 var searchButton = $('#search-button');
 
+//City search variables
+var longitude;
+var latitude;
+
+
+
 
 //Fetch server-side API data
-function getAPI(){
+function getAPI() {
 
     //Setup API URL
     cityName = citySearch.val();
 
+
     var openWeatherAPIkey = "204cd8bda70bb395d61013c2ab681a00";
 
-    var openWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" 
-    + cityName 
-    + "&appid="
-    + openWeatherAPIkey;
+    var openWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q="
+        + cityName
+        + "&appid="
+        + openWeatherAPIkey;
 
+
+
+    //Get latitude and longitude
     fetch(openWeatherURL)
 
-    .then(function (response){
-        console.log(response.status);
-        return response.json();
-       
-    })
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
 
-    .then(function(data){
-        console.log(data);
-    })
+        })
 
-    .catch( function(error){
-        console.log(error);
-    }
-    )
+        .then(function (data) {
+
+            var coordinates = {
+                longitude: data.coord.lon,
+                latitude: data.coord.lat
+            };
+
+            return coordinates;
+
+        })
+
+        .then(function (coordinates) {
+            var openWeatherAPIkey = "204cd8bda70bb395d61013c2ab681a00";
+
+            //Swap the latitude and longitude into the OneCall api
+            var newOpenWeatherURL = 'https://api.openweathermap.org/data/2.5/onecall?lat='
+                + coordinates.latitude + '&lon=' + coordinates.longitude +
+                '&appid=' + openWeatherAPIkey;
+
+            //Request city data
+            fetch(newOpenWeatherURL)
+
+                .then(function (response) {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                })
+
+                .then(function (data) {
+                    console.log(data);
+                })
+
+                .catch(function (error) {
+                    console.log(error);
+                })
+        })
+
+        .catch(function (error) {
+            console.log(error);
+        }
+        )
+
+
 }
+
 
 $(searchButton).on('click', getAPI);
