38fa14e02005f40c08ebacac33218dba6fa45c10
Cody S <cody.scoles@gmail.com>
2022-02-06 20:19:05 -0500

    Implement search history buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 
 //Reference HTML 
+var searchWindow = $('#search-window');
 var citySearch = $("#city-search");
 var searchButton = $('#search-button');
 var currentWeatherDisplay = $('#current-weather');
@@ -12,6 +13,7 @@ var fiveDayForcastHeader = $('#five-day-forecast-header');
 var cityName = '';
 var cityData = {};
 var cityFiveDayData = {};
+var searchHistoryButtons = [];
 
 //Luxon setup
 var DateTime = luxon.DateTime;
@@ -19,10 +21,19 @@ var DateTime = luxon.DateTime;
 
 
 //Fetch server-side API data
-function handleWeatherSearch() {
-
+function handleWeatherSearch(event) {
+    console.log(event.target);
     //Setup API URL to get city coordinates
     cityName = citySearch.val();
+
+    //If the user uses a search history button, get the city name from the button
+    var target = $(event.target);
+
+    console.log(target);
+
+    if(target.text() != ''){
+        cityName = target.text();
+    }
 
 
     var openWeatherAPIkey = "204cd8bda70bb395d61013c2ab681a00";
@@ -43,7 +54,7 @@ function handleWeatherSearch() {
             }
 
         })
-        
+
         //Extract coordinates from city search
         .then(function (data) {
 
@@ -87,15 +98,18 @@ function handleWeatherSearch() {
                     if (currentWeatherDisplay.find('#current-weather-temp').length > 0 |
                         currentWeatherDisplay.find('#current-weather-wind').length > 0 |
                         currentWeatherDisplay.find('#current-weather-humidity').length > 0 |
-                        currentWeatherDisplay.find('#current-weather-uvi').length > 0) {
+                        currentWeatherDisplay.find('#current-weather-uvi').length > 0 |
+                        currentWeatherDisplay.find('#current-weather-uvi-label').length > 0
+                    ) {
 
                         currentWeatherDisplay.find('#current-weather-temp').remove();
                         currentWeatherDisplay.find('#current-weather-wind').remove();
                         currentWeatherDisplay.find('#current-weather-humidity').remove();
                         currentWeatherDisplay.find('#current-weather-uvi').remove();
+                        currentWeatherDisplay.find('#current-weather-uvi-label').remove();
                     }
 
-                //Render HTML elements
+                    //Render HTML elements
                     //Weather icon element
                     var weatherIcon = $('<img>');
                     weatherIcon.attr('alt', cityData.current.weather[0].description);
@@ -103,20 +117,38 @@ function handleWeatherSearch() {
 
                     //Weather data elements
                     var temp = $('<h3>');
-                    temp.text(cityData.current.temp + " °F");
+                    temp.text("Temp: " + cityData.current.temp + " °F");
                     temp.attr('id', 'current-weather-temp');
 
                     var wind = $('<h3>');
-                    wind.text(cityData.current.wind_speed + " MPH");
+                    wind.text("Wind: " + cityData.current.wind_speed + " MPH");
                     wind.attr('id', 'current-weather-wind');
 
                     var humidity = $('<h3>');
-                    humidity.text(cityData.current.humidity + "%");
+                    humidity.text("Humidity: " + cityData.current.humidity + "%");
                     humidity.attr('id', 'current-weather-humidity');
 
                     var uvIndex = $('<h3>');
-                    uvIndex.text(cityData.current.uvi);
-                    uvIndex.attr('id', 'current-weather-uvi');
+                    var uvIndex2 = $('<span>')
+                    uvIndex.text("UV Index: ");
+                    uvIndex.attr('id', 'current-weather-uvi-label');
+                    uvIndex2.text(cityData.current.uvi);
+                    uvIndex2.attr('id', 'current-weather-uvi');
+                    uvIndex.append(uvIndex2);
+
+                    //UV color coding
+                    if (cityData.current.uvi < 3) {
+                        uvIndex2.css('background-color', "green");
+                    }
+
+                    else if (cityData.current.uvi > 3 && cityData.current.uvi < 5) {
+                        uvIndex2.css('background-color', "yellow");
+                    }
+
+                    else {
+                        uvIndex2.css('background-color', "red");
+                    }
+
 
                     //Mem. Add class to styles.css and use addClass method
                     currentWeatherDisplay.css('border', '1px solid black');
@@ -124,7 +156,7 @@ function handleWeatherSearch() {
 
                     //Mem. Add datetime element
                     cityNameDisplay.text(cityName +
-                        " (" +  DateTime.now().toLocaleString() +
+                        " (" + DateTime.fromSeconds(cityData.daily[0].dt).toLocaleString() +
                         ") ").append(weatherIcon);
 
 
@@ -134,25 +166,88 @@ function handleWeatherSearch() {
                     currentWeatherDisplay.append(uvIndex);
 
                     //Clear the 5 Day Forecast
+                    if (fiveDayForecast.find('.card').length > 0) {
 
-                //Render HTML elements
-                    // DateTime.now().plus({ days: 1 }).toLocaleString()
+                        while (fiveDayForecast.find('.card').length > 0) {
+
+                            fiveDayForecast.find('.card').remove();
+                        }
+                    }
+
+                    //Render HTML elements
                     fiveDayForcastHeader.text('Five Day Forecast:')
 
 
-                    for(let i=0; i<5; i++){
+                    for (let i = 1; i < 6; i++) {
                         var day = $('<div>');
                         day.addClass('card');
+                        day.css('background-color', 'lightblue');
 
                         var date = $('<h3>');
-                        date.text(DateTime.now().plus({ days: 1 }).toLocaleString());
+                        date.text(DateTime.fromSeconds(cityData.daily[i].dt).toLocaleString());
                         day.append(date);
+
+                        var weatherIcon = $('<img>');
+                        weatherIcon.attr('alt', cityData.daily[i].weather[0].description);
+                        weatherIcon.attr('src', "http://openweathermap.org/img/wn/"
+                            + cityData.daily[i].weather[0].icon + "@2x.png");
+                        day.append(weatherIcon);
+
+                        var temp = $('<h3>')
+                        temp.text(cityData.daily[i].temp.day + " °F");
+                        temp.attr('class', 'forecasted-weather');
+                        day.append(temp);
+
+                        var wind = $('<h3>')
+                        wind.text(cityData.daily[i].wind_speed + " MPH");
+                        wind.attr('class', 'forecasted-weather');
+                        day.append(wind);
+
+                        var humidity = $('<h3>')
+                        humidity.text(cityData.daily[i].humidity + "%");
+                        humidity.attr('class', 'forecasted-weather');
+                        day.append(humidity);
 
                         fiveDayForecast.append(day);
                     }
-                    
 
-                   
+                    //Append search history button
+
+                    //Clear search history buttons
+                    searchWindow.find('button').remove();
+
+                    var searchHistoryButton = $('<button>');
+                    searchHistoryButton.text(cityName.toUpperCase());
+                    searchHistoryButton.css('margin-bottom', '10px');
+
+                    //Check for a duplicate button
+                    var isDuplicate = false;
+                    for (let i = 0; i < searchHistoryButtons.length; i++) {
+                        if (searchHistoryButtons[i].text() == searchHistoryButton.text()) {
+                            isDuplicate = true;
+                        }
+                    }
+
+                    if (isDuplicate === false) {
+                        //Only allow a total of ten search history buttons
+                        if (searchHistoryButtons.length < 10) {
+                            searchHistoryButtons.unshift(searchHistoryButton);
+                        }
+                        else {
+                            searchHistoryButtons.pop();
+                            searchHistoryButtons.unshift(searchHistoryButton);
+                        }
+                    }
+
+                    searchWindow.append(searchHistoryButtons);
+
+                    //Save searched cities to local storage
+                    var cityNames = [];
+                    for(let i=0; i < searchHistoryButtons.length; i++){
+                        cityNames.unshift(searchHistoryButtons[i].text());
+                    }
+
+                    localStorage.setItem('searchedCities', cityNames);
                 })
 
                 .catch(function (error) {
@@ -170,5 +265,23 @@ function handleWeatherSearch() {
 
 }
 
+//Render previous searches
+function renderPreviousSearches(){
+    var cityNames = localStorage.getItem('searchedCities');
+    cityNames = cityNames.split(',');
+
+    //Mem. write fxn to handle button creation
+    for(let i=0; i < cityNames.length; i++){
+        var searchHistoryButton = $('<button>');
+        searchHistoryButton.text(cityNames[i]);
+        searchHistoryButton.css('margin-bottom', '10px');
+        searchHistoryButtons.unshift(searchHistoryButton);
+
+        searchWindow.append(searchHistoryButtons);
+    }
+}
+
+renderPreviousSearches();
 
 $(searchButton).on('click', handleWeatherSearch);
+$(searchWindow).on('click', 'button', handleWeatherSearch);
